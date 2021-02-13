121009feedbdb0d6e350f6a6f288422623e0dece
Madelyn Mathai <madelynmathai@GEORGEs-MacBook-Air.local>
2021-02-13 12:15:34 -0800

    mission to mars

---
+++ Mission_to_Mars.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+
+# Standard dependencies
+import pandas as pd
+
+
+
+# Mac users Madelyn path
+executable_path = {'executable_path': '/Users/madelynmathai/.wdm/drivers/chromedriver/mac64/88.0.4324.96/chromedriver'}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# Visit the mars nasa news site
+url = 'https://mars.nasa.gov/news/'
+browser.visit(url)
+# Optional delay for loading the page
+browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+
+
+html = browser.html
+news_soup = soup(html, 'html.parser')
+slide_elem = news_soup.select_one('ul.item_list li.slide')
+
+
+
+slide_elem.find("div", class_='content_title')
+
+
+# Use the parent element to find the first `a` tag and save it as `news_title`
+news_title = slide_elem.find("div", class_='content_title').get_text()
+news_title
+
+
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+news_p
+
+
+
+# Visit URL
+url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+browser.visit(url)
+
+
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+
+
+# Find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+img_url_rel
+
+
+
+# Use the base URL to create an absolute URL
+img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+img_url
+
+
+
+
+df = pd.read_html('http://space-facts.com/mars/')[0]
+df.columns=['description', 'value']
+df.set_index('description', inplace=True)
+df
+
+
+
+df.to_html()
+
+
+
+browser.quit()
+
+
+
+
+
